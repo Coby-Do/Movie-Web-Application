2ce82f61b0df9c6b3470940082ea1a1f7e593430
Luis Gonzalez <gonzalezdavidr22@gmail.com>
2023-02-12 23:14:25 -0800

    Basic Proof of Concept With Fetching Data from API and Template Setup

--- theatrelob/home/views.py
+++ theatrelob/home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+import json
+import tmdbsimple as tmdb
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the theatrelob index.")
+    # display a movie from tmbd api
+    with open('secrets.json') as f:
+        secrets = json.load(f)
+        tmdb.API_KEY = secrets['tmdb_api_key']
+    movie = tmdb.Movies(550)
+    movie.info()
+    # get image url
+    poster_url = 'https://image.tmdb.org/t/p/w500' + movie.poster_path
+    return render(request, 'home/index.html', {'movie_title': movie.title, 'movie_overview': movie.overview, 
+    'movie_poster': poster_url, 'movie_backdrop': movie.backdrop_path})
--- theatrelob/services/tmdb_api.py
+++ theatrelob/services/tmdb_api.py
@@ -1,4 +1,5 @@
 
+import json
 import tmdbsimple as tmdb
 #read secrets.json file one dir up
 with open('../secrets.json') as f:
