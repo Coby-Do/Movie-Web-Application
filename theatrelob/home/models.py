531046cdf5efd73b616330b982c419a9b32106ee
Coby-Do <111834642+Coby-Do@users.noreply.github.com>
2023-02-14 00:18:26 -0800

    Update models.py

--- theatrelob/home/models.py
+++ theatrelob/home/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 # from pinax.badges.base import Badge
 
 class Movie(models.Model):
