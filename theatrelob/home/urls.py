e153d71658bfa7f9294c7acdd1fce4a8e445444a
Luis Gonzalez <luigon@colmac.com>
2023-02-13 19:56:53 -0800

    user auth and start of watchlist

--- theatrelob/home/urls.py
+++ theatrelob/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('watchlist', views.watchlist, name='watchlist'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='home/login.html')),
 ]
--- theatrelob/home/views.py
+++ theatrelob/home/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 import json
 import tmdbsimple as tmdb
-# Create your views here.
+from django.shortcuts import redirect
 from .models import Movie, Profile, WatchedItem
+from django.contrib.auth.decorators import login_required
+
 
 
 def index(request):
@@ -23,3 +25,43 @@ def index(request):
     print(list_of_movies)
     # get image url
     return render(request, 'home/index.html', {'movies': list_of_movies})
+
+@login_required(login_url='accounts/login/')
+def watchlist(request):
+    # retrieve all watched items from the database belonging to the user
+    # display them on the page
+    # get the user's id
+    user_id = request.user.id
+
+    # get the user's profile
+    profile = Profile.objects.get(user_id=user_id)
+    # get the user's watched items
+    watched_items = WatchedItem.objects.filter(user=profile.user)
+    
+    # display the watched items
+    return render(request, 'home/watchlist.html', {'watched_items': watched_items})
+    
+def add_to_watchlist(request):
+    # get the user's id
+    user_id = request.user.id
+
+    # get the user's profile
+    profile = Profile.objects.get(user_id=user_id)
+    # get the movie's id
+    movie_id = request.POST['movie_id']
+    #if it doesn't exist, create it
+    if not Movie.objects.filter(id=movie_id).exists():
+        with open('secrets.json') as f:
+            secrets = json.load(f)
+            tmdb.API_KEY = secrets['tmdb_api_key']
+        movie = tmdb.Movies(movie_id).info()
+        poster_url = 'https://image.tmdb.org/t/p/w500' + movie['poster_path']
+        m = Movie(id=movie_id, title=movie['title'], description=movie['overview'], movie_poster_url=poster_url)
+        m.save()
+    # get the movie
+    movie = Movie.objects.get(id=movie_id)
+    # add the movie to the user's watchlist
+    watched_item = WatchedItem(user=profile.user, movie=movie)
+    watched_item.save()
+    # redirect to the watchlist page
+    return redirect('watchlist')
