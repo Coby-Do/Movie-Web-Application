af45e83a950cb3519379267f2a5836441841bfb5
bobochump <jasonbui2001@gmail.com>
2023-04-17 21:23:56 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/Coby-Do/Movie-Web-Application""
    This reverts commit 06051002e1fdaf673365da79230bf5cf9549b3ba.

--- theatrelob/home/models.py
+++ theatrelob/home/models.py
@@ -11,7 +11,12 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
 
 
-# Create your models here.
+# Each genre can be assocciated with multiple movies and each movie can be associated with multiple genres
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
@@ -25,7 +30,9 @@ class Movie(models.Model):
     gross_earning_in_mil = models.IntegerField(null=True, blank=True)
     director = models.CharField(max_length=100, null=True, blank=True)
     actor = models.CharField(max_length=100, null=True, blank=True)
-    genre = models.CharField(max_length=100, null=True, blank=True)
+    
+    genres = models.ManyToManyField(Genre)
+
     language = models.CharField(max_length=100, null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
     tmdb_id = models.IntegerField()
@@ -33,80 +40,163 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
+# Represents the badge objects and initializes the badge data to a database
 class Badge(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    requirement = models.PositiveIntegerField(default=0)
-    badge_type = models.CharField(max_length=255, null=True, blank=True)
-    
+    # Defines the name, description, badge_type, genre as characters
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    badge_type = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50, blank=True)
+
+    # Defines the requirement as an integer
+    requirement = models.IntegerField()
+
+    # Defines a many-to-many relationship between the Badge and UserProfile models
+    # (target model, allows the UserProfile to access the earned badges, allows the UserProfile to access each badge, allows badge to exist without associating with a user)
+    users = models.ManyToManyField('UserProfile', related_name='earned_badges', related_query_name='badge', blank=True)
+
+    # Returns the badge name
     def __str__(self):
         return self.name
-    
 
+# Properply formatting the genre's name
+def format_genre_name(genre):
+    if genre.lower() == "tv movie":
+        return "TV Movie"
+    elif genre.lower() == "science fiction":
+        return "Science Fiction"
+    else:
+        return genre.title()
+
+# Represents the UserProfile objects and initializes the badge data to a database
 class UserProfile(models.Model):
+    # Defines the user to a single user, the UserProfile's instance will be deleted if the User's instance is deleted
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True, null=True)  # Added bio field from Profile class
+
+    # Defines the number of movies watched as an integer
     movies_watched = models.PositiveIntegerField(default=0)
-    animated_movies_watched = models.PositiveIntegerField(default=0)
-    documentaries_watched = models.PositiveIntegerField(default=0)
+
+    # Defines the genres as integers
     action_movies_watched = models.PositiveIntegerField(default=0)
+    adventure_movies_watched = models.PositiveIntegerField(default=0)
+    animation_movies_watched = models.PositiveIntegerField(default=0)
     comedy_movies_watched = models.PositiveIntegerField(default=0)
+    crime_movies_watched = models.PositiveIntegerField(default=0)
+    documentary_movies_watched = models.PositiveIntegerField(default=0)
+    drama_movies_watched = models.PositiveIntegerField(default=0)
+    family_movies_watched = models.PositiveIntegerField(default=0)
+    fantasy_movies_watched = models.PositiveIntegerField(default=0)
+    history_movies_watched = models.PositiveIntegerField(default=0)
+    horror_movies_watched = models.PositiveIntegerField(default=0)
+    music_movies_watched = models.PositiveIntegerField(default=0)
+    mystery_movies_watched = models.PositiveIntegerField(default=0)
     romance_movies_watched = models.PositiveIntegerField(default=0)
+    science_fiction_movies_watched = models.PositiveIntegerField(default=0)
+    thriller_movies_watched = models.PositiveIntegerField(default=0)
+    tv_movie_movies_watched = models.PositiveIntegerField(default=0)
+    war_movies_watched = models.PositiveIntegerField(default=0)
+    western_movies_watched = models.PositiveIntegerField(default=0)
+
+    # Defines the badges with the badge objects
     badges = models.ManyToManyField(Badge)
 
+    # Returns the username
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return self.user.username
 
+    # Awards the user with the badge
     def award_badge(self, badge):
         self.badges.add(badge)
         self.save()
 
     def check_badges(self):
+        # List to store the newly earned badges
         newly_earned_badges = []
+        # Gets all the Badge objects from the database
         badges = Badge.objects.all()
-        genres_watched = self.update_genres_watched()
+
+        # Get watched movies and count genres
+        genre_counts = {}
+
+        # Iterate through watched movies associated with the user profile
+        for watched_movie in self.watcheditem_set.all():
+            # Checks if the movie has a genre associated with it
+            if watched_movie.movie.genres.exists():
+                # Creates a list of genre names for the current movie
+                genres = []
+                for genre in watched_movie.movie.genres.all():
+                    genres.append(genre.name)
+
+                # Updates the genre_counts dictionary 
+                for genre in genres:
+                    if genre in genre_counts:
+                        genre_counts[genre] += 1
+                    else:
+                        genre_counts[genre] = 1
+
+        # Check if the user meets the badge requirements based on the genre counts
         for badge in badges:
-            if badge.badge_type == 'movies_watched':
-                if self.movies_watched >= badge.requirement and badge not in self.badges.all():
+            # Checking for the correct badge type, validating badge requirement and whether the user already possesses the badge
+            if badge.badge_type == 'movies_watched' and self.movies_watched >= badge.requirement and badge not in self.badges.all():
+                self.badges.add(badge)
+                newly_earned_badges.append(badge)
+            elif badge.badge_type == 'genre':
+                # Get the genre name associated with the badge
+                genre = badge.genre
+                # Format the genre name to match the defined objects from the UserProfile class
+                formatted_genre = format_genre_name(genre)
+                formatted_genre_name = formatted_genre.lower().replace(' ', '_')
+                genre_object_name = formatted_genre_name + "_movies_watched"
+
+                # Getting the count of watched movies for the current genre
+                genre_count = getattr(self, genre_object_name, 0)
+                # Validating badge requirement and whether the user already possesses the badge
+                if genre_count >= badge.requirement and badge not in self.badges.all():
                     self.badges.add(badge)
                     newly_earned_badges.append(badge)
-            elif badge.badge_type == 'genres_watched':
-                if genres_watched >= badge.requirement and badge not in self.badges.all():
-                    self.badges.add(badge)
-                    newly_earned_badges.append(badge)
-            elif badge.badge_type == 'animated_movies_watched':
-                if self.animated_movies_watched >= badge.requirement and badge not in self.badges.all():
-                    self.badges.add(badge)
-                    newly_earned_badges.append(badge)
-            elif badge.badge_type == 'documentaries_watched':
-                if self.documentaries_watched >= badge.requirement and badge not in self.badges.all():
-                    self.badges.add(badge)
-                    newly_earned_badges.append(badge)
-            elif badge.badge_type == 'action_movies_watched':
-                if self.action_movies_watched >= badge.requirement and badge not in self.badges.all():
-                    self.badges.add(badge)
-                    newly_earned_badges.append(badge)
-            elif badge.badge_type == 'comedy_movies_watched':
-                if self.comedy_movies_watched >= badge.requirement and badge not in self.badges.all():
-                    self.badges.add(badge)
-                    newly_earned_badges.append(badge)
-            elif badge.badge_type == 'romance_movies_watched':
-                if self.romance_movies_watched >= badge.requirement and badge not in self.badges.all():
-                    self.badges.add(badge)
-                    newly_earned_badges.append(badge)
-        self.save()
+
+        # Return the list of newly earned badges
         return newly_earned_badges
-    
-    def update_genres_watched(self):
-        genre_counts = [
-            self.animated_movies_watched,
-            self.documentaries_watched,
-            self.comedy_movies_watched,
-            self.action_movies_watched,
-            self.romance_movies_watched,
-        ]
-        genres_watched = sum(1 for count in genre_counts if count > 0)
-        return genres_watched
+
+    def update_genres_watched(self, genre):
+        # Format genre name
+        genre = format_genre_name(genre)
+
+        # Mapping of genre names to UserProfile model objects
+        genre_to_object = {
+            "Action": "action_movies_watched",
+            "Adventure": "adventure_movies_watched",
+            "Animation": "animation_movies_watched",
+            "Comedy": "comedy_movies_watched",
+            "Crime": "crime_movies_watched",
+            "Documentary": "documentary_movies_watched",
+            "Drama": "drama_movies_watched",
+            "Family": "family_movies_watched",
+            "Fantasy": "fantasy_movies_watched",
+            "History": "history_movies_watched",
+            "Horror": "horror_movies_watched",
+            "Music": "music_movies_watched",
+            "Mystery": "mystery_movies_watched",
+            "Romance": "romance_movies_watched",
+            "Science Fiction": "science_fiction_movies_watched",
+            "Thriller": "thriller_movies_watched",
+            "TV Movie": "tv_movie_movies_watched",
+            "War": "war_movies_watched",
+            "Western": "western_movies_watched",
+        }
+
+        # Get the corresponding UserProfile model field for the genre
+        object_name = genre_to_object.get(genre)
+
+        if object_name:
+            # Get the current value of the genre count
+            current_value = getattr(self, object_name)
+
+            # Update the genre count by adding 1
+            setattr(self, object_name, current_value + 1)
+
+            # Save the updated UserProfile model
+            self.save()
 
     # For recommendations list
     def recommend_movies(self, movie, numofmovies=5):
@@ -117,6 +207,33 @@ class UserProfile(models.Model):
         recommender = MovieRecommender(credits_file, movies_file)  # Creates movie recommender object
         recommend_movies = recommender.recommend(movie)  # Calls recommender to recommend a movie
         return recommend_movies
+
+class WatchedItem(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date_watched = models.DateField()
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.movie.title
+
+# create a model representing a third party integrationss
+class Integration(models.Model):
+    name = models.CharField(max_length=100)
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
+    access_token = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
     
 class MovieRecommender:
     def __init__(self, creditsfile, moviefile):
@@ -198,11 +315,9 @@ class MovieRecommender:
 
     # Function that prepares the data to be input into the vectorizer. Takes feature as input
     def prepvectorizer(self, features):
-        return ' '.join(features['keywords']) + ' ' + ' '.join(features['cast']) + ' ' + features[
-            'director'] + ' ' + ' '.join(features['genres'])
+        return ' '.join(features['keywords']) + ' ' + ' '.join(features['cast']) + ' ' + features['director'] + ' ' + ' '.join(features['genres'])
 
     def plotrec(self, movieslist):
-
         if not (self.moviefile['title'].eq(movieslist).any()):
             return None
 
@@ -221,40 +336,3 @@ class MovieRecommender:
         moviesindices = [ind[0] for ind in simscore]
         movies = self.moviefile["title"].iloc[moviesindices]
         return movies
-
-
-
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     bio = models.TextField()
-
-#     def __str__(self):
-#         return self.user.username
-
-class WatchedItem(models.Model):
-    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    date_watched = models.DateField()
-
-    def __str__(self):
-        return self.movie.title
-
-# create a model representing a third party integrationss
-class Integration(models.Model):
-    name = models.CharField(max_length=100)
-    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
-    access_token = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
