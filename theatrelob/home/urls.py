06051002e1fdaf673365da79230bf5cf9549b3ba
bobochump <jasonbui2001@gmail.com>
2023-04-17 21:16:44 -0700

    Revert "Merge branch 'main' of https://github.com/Coby-Do/Movie-Web-Application"
    This reverts commit eff6af315857994c36ce4242ec8f75494d788d2c, reversing
    changes made to 9845b76c41d611e7681a754f8a08ee56c6e68b72.

--- theatrelob/home/urls.py
+++ theatrelob/home/urls.py
@@ -5,28 +5,28 @@ from .views import logout_view
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='home/login.html')),
-
-    # For Watchlists
     path('watchlist/', views.watchlist, name='watchlist'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='home/login.html')),
     path('add_to_watchlist', views.add_to_watchlist, name='add_to_watchlist'),
     path('get_access_token', views.get_access_token, name='get_access_token'),
-
-    # For Profiles, Logins, Logouts, Registrations
+    path('randomrec/', views.randomrec, name='Random Movie Recommendation'),
+    # Handling profiles and badges
     path('logout/', logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
-
-    # For Badges
     path('badge_list/', views.badge_list, name='badge_list'),
+    path('watch_movie/', views.watch_movie, name='watch_movie'),
     path('create_badges/', views.create_badges, name='create_badges'),
     path('reset_user_badges/', views.reset_user_badges, name='reset_user_badges'),
     path('delete_badges/', views.delete_badges, name='delete_badges'),
-
+    path('watch_animation/', views.watch_animation, name='watch_animation'),
+    path('watch_documentary/', views.watch_documentary, name='watch_documentary'),
+    path('watch_action/', views.watch_action, name='watch_action'),
+    path('watch_comedy/', views.watch_comedy, name='watch_comedy'),
+    path('watch_romance/', views.watch_romance, name='watch_romance'),
     # For Recommending Movies
-    path('randomrec/', views.randomrec, name='Random Movie Recommendation'),
     path('recommend_movie_view/', views.recommend_movie_view, name='recommendList'),
     path('movie_search_add/', views.movie_search_add, name='addedMovie'),
 ]
